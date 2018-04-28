0808326484c8e8ee827ddb6e462499a3fa4e1513
tschultz1216 <tschultz@unca.edu>
2018-04-28 01:19:39 -0400

    Upgraded to open source implementation with ^0.4.2

--- ERC735.sol
+++ ERC735.sol
@@ -1,23 +1,48 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
-contract ERC735 {
+import "./ERC165.sol";
 
-    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);    event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, address indexed issuer, uint256 signatureType, bytes signature, bytes claim, string uri);
+/// @title ERC735
+/// @author Mircea Pasoi
+/// @notice Abstract contract for ERC735
+contract ERC735 is ERC165 {
+    /// @dev ID for ERC165 pseudo-introspection
+    /// @return ID for ERC725 interface
+    function ERC735ID() public pure returns (bytes4) {
+        return (
+            this.getClaim.selector ^ this.getClaimIdsByType.selector ^
+            this.addClaim.selector ^ this.removeClaim.selector
+        );
+    }
+
+    /// @dev Constructor that adds ERC735 as a supported interface
+    constructor() internal {
+        supportedInterfaces[ERC735ID()] = true;
+    }
+
+    // ClaimType
+    uint256 constant BIOMETRIC_CLAIM = 1; // you're a person and not a business
+    uint256 constant RESIDENCE_CLAIM = 2; // you have a physical address or reference point
+    uint256 constant REGISTRY_CLAIM = 3;
+    uint256 constant PROFILE_CLAIM = 4; // TODO: social media profiles, blogs, etc.
+    uint256 constant LABEL_CLAIM = 5; // TODO: real name, business name, nick name, brand name, alias, etc.
+
+    // Scheme
+    uint256 constant ECDSA_SCHEME = 1;
+    // https://medium.com/@alexberegszaszi/lets-bring-the-70s-to-ethereum-48daa16a4b51
+    uint256 constant RSA_SCHEME = 2;
+    // 3 is contract verification, where the data will be call data, and the issuer a contract address to call
+    uint256 constant CONTRACT_SCHEME = 3;
+
+    // Events
+    event ClaimRequested(uint256 indexed claimRequestId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
     event ClaimAdded(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
     event ClaimRemoved(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
     event ClaimChanged(bytes32 indexed claimId, uint256 indexed claimType, uint256 scheme, address indexed issuer, bytes signature, bytes data, string uri);
 
-    struct Claim {
-        uint256 claimType;
-        uint256 scheme;
-        address issuer; // msg.sender
-        bytes signature; // this.address + claimType + data
-        bytes data;
-        string uri;
-    }
-
-    function getClaim(bytes32 _claimId) public constant returns(uint256 claimType, uint256 scheme, address issuer, bytes signature, bytes data, string uri);
-    function getClaimIdsByType(uint256 _claimType) public constant returns(bytes32[] claimIds);
-    function addClaim(uint256 _claimType, uint256 _scheme, address _issuer, bytes _signature, bytes _data, string _uri) public returns (uint256 claimRequestId);
+    // Functions
+    function getClaim(bytes32 _claimId) public view returns(uint256 claimType, uint256 scheme, address issuer, bytes signature, bytes data, string uri);
+    function getClaimIdsByType(uint256 _claimType) public view returns(bytes32[] claimIds);
+    function addClaim(uint256 _claimType, uint256 _scheme, address issuer, bytes _signature, bytes _data, string _uri) public returns (uint256 claimRequestId);
     function removeClaim(bytes32 _claimId) public returns (bool success);
 }
